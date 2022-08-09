c38178438f7a44255ded86e80831c95cfd7a751e
carlosayol <Carlosaolar@gmail.com>
2022-08-08 19:55:57 -0500

    make comments

--- channels.go
+++ channels.go
@@ -1,9 +1,9 @@
 package main
 
-ch := make(chan T)
+// ch := make(chan T)
 
 // value v is going into the channel ch
-ch <- v
+// ch <- v
 
 // v is receiving the values from the channel ch
-v = <- ch
+// v = <- ch
